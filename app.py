cd6607cc5ecb045d0a5bcb98acca1c70e2fdfc52
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-01-25 06:23:49 +0000

    Backbone creation

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__, static_folder="./static")
 
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def index():
+    if request.method == 'POST':
+        name = request.form["name"]
+        print(name)
+        return redirect('match.html')
+        
     return render_template("index.html")
+
+
+# FORM DATA
+@app.route('/match', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        name = request.form["name"]
+        print(name)
+        return render_template('match.html')
